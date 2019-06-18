ca2c7f32b908db32268828ec5e9da738ac3b76fc
Tomoya Ogawa <ot1019ot@gmail.com>
2019-06-18 20:51:18 +0900

    [Finish]5-4

--- Gemfile
+++ Gemfile
@@ -65,3 +65,6 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 
 gem 'devise'
+
+gem 'kaminari','~> 1.1.1'
+
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'bf02eb61cba5dafcf40bf582d9da00ff78bab7b5769f692bc0ed0d26082b004d9d0cd04289f795271869caec1001a5246c3e85ac88d1b258e01747c34dd57fec'
